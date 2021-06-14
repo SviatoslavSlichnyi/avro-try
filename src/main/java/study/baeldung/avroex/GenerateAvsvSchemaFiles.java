9aa01d2cd276251d6d797249a7cf1adf43d82444
Sviatoslav_Slichnyi <sslichnyi@gmail.com>
2021-06-14 17:53:38 +0300

    Generate avsc files which contains schemas for generation an pojo classes. Using: SchemaBuilder

---
+++ src/main/java/study/baeldung/avroex/GenerateAvsvSchemaFiles.java
@@ -0,0 +1,59 @@
+package study.baeldung.avroex;
+
+import org.apache.avro.Schema;
+import org.apache.avro.SchemaBuilder;
+import study.baeldung.avroex.common.Constrains;
+import study.baeldung.avroex.common.FileUtil;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class GenerateAvsvSchemaFiles {
+    public static void main(String[] args) throws IOException {
+        Schema clientIdentifierSchema = SchemaBuilder.record("ClientIdentifier")
+                .namespace("study.baeldung.avroex.entity")
+                .fields()
+                    .requiredString("hostName")
+                    .requiredString("ipAddress")
+                .endRecord();
+
+        System.out.println("ClientIdentifier:");
+        System.out.println(clientIdentifierSchema.toString());
+
+        Schema avroHttpRequestSchema = SchemaBuilder.record("AvroHttpRequest")
+                .namespace("study.baeldung.avroex.entity")
+                .fields()
+                    .requiredLong("requestTime")
+                    .name("clientIdentifier")
+                    .   type(clientIdentifierSchema)
+                        .noDefault()
+                    .name("employeeNames")
+                        .type().array()
+                        .items().stringType()
+                        .noDefault()
+                    .name("active")
+                        .type().enumeration("Active")
+                        .symbols("YES", "NO")
+                        .noDefault()
+                    .endRecord();
+
+        System.out.println("AvroHttpRequest:");
+        System.out.println(avroHttpRequestSchema.toString());
+
+        // saving to src/main/resources/avsc/
+        FileUtil.saveArscToFile(clientIdentifierSchema.toString(), Constrains.clientIdentifierSchemaFilePath);
+        FileUtil.saveArscToFile(avroHttpRequestSchema.toString(), Constrains.avroHttpRequestSchemaFilePath);
+
+        System.out.println("----------------------");
+
+        String clientIdentifierSchemaFromFile = Files.readString(Constrains.clientIdentifierSchemaFilePath);
+        String avroHttpRequestSchemaFromFile = Files.readString(Constrains.avroHttpRequestSchemaFilePath);
+
+        System.out.println("ClientIdentifier from File:");
+        System.out.println(clientIdentifierSchemaFromFile);
+        System.out.println("AvroHttpRequest from File:");
+        System.out.println(avroHttpRequestSchemaFromFile);
+    }
+}
